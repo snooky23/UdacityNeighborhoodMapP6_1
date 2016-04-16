54deb2a68526d0b405d6d757aa90b2c813deb015
Avi Levinshtein <avi.levinshtein@ironsrc.com>
2016-04-16 18:54:35 +0300

    Filter Locations via Text Input

--- scripts/viewModel.js
+++ scripts/viewModel.js
@@ -7,13 +7,29 @@ var Location = function(data) {
 var ViewModel = function() {
 	var self = this;
 	this.locationList = ko.observableArray([]);
-	//iterate over location list from map.js
+	this.filter = ko.observable("");
+
+	//Get locations from map.js
 	locations.forEach(function(locationItem){
 		self.locationList.push(new Location(locationItem));
 	});
-	//point to the first location in the list
-	this.currentLocation = ko.observable(this.locationList()[0]);
+	
+	//Bind search filter with the listview component
+	this.visiblePlaces = ko.computed(function(){
+       return this.locationList().filter(function(place){
+           if(!self.filter() || place.name().toLowerCase().indexOf(self.filter().toLowerCase()) !== -1)
+             return place;
+       });
+   	},this);  
 
+   	//point to the first location in the list
+	this.currentLocation = ko.observable(this.visiblePlaces()[0]);
+
+	//Support clicked location event
+	this.setLocation = function(clickedLocation) {
+		self.currentLocation(clickedLocation);
+		console.log(self.currentLocation().name());
+	};
 }
 
 $( document ).ready( function() {
